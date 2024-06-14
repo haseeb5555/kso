865dc1af740cecef5e49dfccc8971ba95c7c07ef
unknown <haseebmoon666@gmail.com>
2024-06-14 21:12:29 +0500

    teacher module

--- src/pages/TeacherProfile/teacher-profile.tsx
+++ src/pages/TeacherProfile/teacher-profile.tsx
@@ -3,13 +3,63 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import TeaNav from "@/components/teaNav";
 import Footer from "@/components/footer";
+import { EditIcon } from "lucide-react";
 
 const TeacherProfile = () => {
     const [teacher, setTeacher] = useState({
+        name: "",
         phone: "",
         email: "",
         address: "",
     });
+    const [courses, setCourses] = useState([
+        // dummy data
+        {
+            id: 1,
+            title: "Python",
+            description: "Python is a programming language that lets you work quickly and integrate systems more effectively.",
+            enrolledStudents: 560,
+        },
+        {
+            id: 2,
+            title: "JavaScript",
+            description: "JavaScript is a programming language that lets you work quickly and integrate systems more effectively.",
+            enrolledStudents: 50,
+        },
+        {
+            id: 3,
+            title: "React",
+            description: "React is a JavaScript library for building user interfaces. React makes it painless to create interactive UIs.",
+            enrolledStudents: 260,
+        }
+
+    ]);
+    const [oldCourses, setOldCourses] = useState([{
+        id: 1,
+        title: "Python",
+        description: "Python is a programming language that lets you work quickly and integrate systems more effectively.",
+        enrolledStudents: 960,
+    }, 
+    {
+        id: 2,
+        title: "JavaScript",
+        description: "JavaScript is a programming language that lets you work quickly and integrate systems more effectively.",
+        enrolledStudents: 560,
+    },
+
+    {
+        id: 3,
+        title: "React",
+        description: "React is a JavaScript library for building user interfaces. React makes it painless to create interactive UIs.",
+        enrolledStudents: 560,
+    },
+  
+    
+
+]);
+
+
+
     const navigate = useNavigate();
 
     const fetchTeacher = async () => {
@@ -18,82 +68,112 @@ const TeacherProfile = () => {
                 withCredentials: true,
             });
             setTeacher(response.data);
+            setCourses(response.data.courses || []); // Assuming courses are part of the teacher data
         } catch (error) {
-            navigate('/login');
+            // navigate('/login');
         }
     };
 
     useEffect(() => {
         fetchTeacher();
     }, []);
+
     if(!teacher) return null;
+
     return (
         <>
-            <TeaNav  />
+            <TeaNav />
             <img className="w-full mt-5" alt="header banner" src="/header-copy-2@2x.png" />
-        <div className="mx-20 ">
-            <h1 className="text-2xl font-bold mt-5">Merhaba</h1>
-            <div className="my-5 flex flex-col md:flex-row items-start md:items-center">
-                <div className="flex items-center mb-5 md:mb-0 md:mr-5">
-                    <div className="w-80 h-80 bg-gray-200 rounded-full flex items-center justify-center  max-sm:ml-28">
-                        <img className="w-10 h-10" alt="profile icon" src="/vector.svg" />
+            <div className="px-20 max-sm:px-4">
+                <h1 className="text-2xl font-bold mt-5">Merhaba {teacher.name}</h1>
+                <div className="my-5 flex flex-col md:flex-row items-start md:items-center">
+                    <div className="flex items-center mb-5 md:mb-0 md:mr-5">
+                        <div className="w-80 h-80 bg-gray-200 rounded-full flex items-center justify-center max-sm:h-40 max-sm:w-40">
+                            <img className="w-10 h-10" alt="profile icon" src="/vector.svg" />
+                        </div>
+                    </div>
+                    <div className="w-full flex justify-between gap-16 max-sm:flex-col">
+                        <div className="text-lg">
+                            <p>
+                                <span className="font-bold">İletişim:</span> {teacher.phone}
+                            </p>
+                            <p>
+                                <span className="font-bold">Email:</span> {teacher.email}
+                            </p>
+                            <p>
+                                <span className="font-bold">Adres:</span> {teacher.address}
+                            </p>
+                        </div>
+                        <div>
+                            <div className="w-full flex justify-between">
+                                <label className="block mb-2 text-lg font-bold">Hakkında</label>
+                                <img className="w-4" alt="profile icon" src="/vector1.svg" />
+                            </div>
+                            <textarea className="w-[400px] max-sm:w-full p-2 border rounded h-24" readOnly />
+                        </div>
                     </div>
                 </div>
-            
-            <div className="w-full flex justify-between  gap-16 max-sm:flex-col" >
-               <div className="text-lg">
-                    <p>
-                        <span className="font-bold">İletişim:</span> {teacher.phone}
-                    </p>
-                    <p>
-                        <span className="font-bold">Email:</span> {teacher.email}
-                    </p>
-                    <p>
-                        <span className="font-bold">Adres:</span> {teacher.address}
-                    </p>
-                </div>
                 <div>
-               <div className="w-full flex justify-between">
-                <label className="block mb-2 text-lg font-bold">Hakkında</label>
-                <img className="w-4 " alt="profile icon" src="/vector1.svg" />
-               </div>
-                <textarea className="w-[400px] p-2 border rounded h-24"  readOnly />
-                </div>
-            </div>
-            </div>
-            <div>
-                <div className="flex flex-col justify-center items-center ">
-                <div className="flex justify-between items-center  ">
-
-                <h1 className="text-2xl font-bold mb-5">Açılan kurslar</h1>
-                {/* <div className="mt-5 flex gap-2 items-center cursor-pointer">
-                    <span className="text-blue-500 ">Ekle</span>
-                    <img className="w-5 h-5" alt="add icon" src="/vector3.svg" />
-                </div> */}
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 mx-60 max-sm:mx-0">
-                    {/* {Array(6).fill().map((_, index) => (
-                        <div key={index} className="w-[250px] border flex flex-col border-black pb-5 rounded-lg gap-2">
-                            <div className=" bg-gray-200 px-20 py-20 border border-black"/>
-                            <h3 className="text-lg font-bold mb-2 ml-4">Graphic Design 101</h3>
-                            <p className="mb-2 text-sm ml-4">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            </p>
-                            <button className="ml-4 bg-gray-200 text-white py-2 px-4 rounded w-[100px]">
-                                Görüntüle
-                            </button>
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="flex justify-between items-center">
+                            <h1 className="text-2xl font-bold mb-5">Offering Courses</h1>
                         </div>
-                    ))} */}
+                        <div className="mt-20 mb-20 flex justify-center items-center gap-16 max-sm:flex-col flex-wrap ">
+                            {courses.map((course, index) => (
+                                <div key={index} className="relative w-[300px] border flex flex-col  pb-5 rounded-3xl gap-2 ">
+                                    <div className="bg-gray-200 px-20 py-20 border border-black rounded-3xl"/>
+                                    <h3 className="text-lg font-bold mb-2 ml-4">{course.title}</h3>
+                                    <p className="mb-2 text-sm ml-4">
+                                        {course.description}
+                                    </p>
+                                    <div className="flex justify-between items-center gap-4 px-4">
+
+                                    <button className="w-full bg-gray-200 text-black py-2 px-4 rounded-2xl ">
+                                        Görüntüle
+                                    </button>
+                                    <button className="w-full bg-blue-700 text-white py-2 px-4 rounded-2xl ">
+                                     
+                                        {course.enrolledStudents}&nbsp;<span>Öğrenci</span>
+                                    </button>
+                                    </div>
+                                    <button onClick={() => navigate(`/editCourse`)} className="absolute top-2 right-2">
+                                     <EditIcon size={24} />
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <h1 className="text-2xl font-bold mb-5">Old Courses</h1>
+                            {/* <div className="mt-5 flex gap-2 items-center cursor-pointer">
+                                <span className="text-blue-500 ">Ekle</span>
+                                <img className="w-5 h-5" alt="add icon" src="/vector3.svg" />
+                            </div> */}
+                        </div>
+                        <div className="mt-20 mb-20 flex justify-center items-center gap-16 max-sm:flex-col flex-wrap ">
+                            {oldCourses.map((course, index) => (
+                                <div key={index} className="relative w-[300px] border flex flex-col  pb-5 rounded-3xl gap-2 ">
+                                    <div className="bg-gray-200 px-20 py-20 border border-black rounded-3xl"/>
+                                    <h3 className="text-lg font-bold mb-2 ml-4">{course.title}</h3>
+                                    <p className="mb-2 text-sm ml-4">
+                                        {course.description}
+                                    </p>
+                                    <button className="ml-4 bg-gray-200 text-black py-2 px-4 rounded w-[100px]">
+                                        Görüntüle
+                                    </button>
+                                    <button onClick={() => navigate(`/editCourse`)} className="absolute top-2 right-2">
+                                     <EditIcon size={24} />
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                        
+                    </div>
                 </div>
-                </div>
-             
             </div>
-          
-        </div>
-           <Footer/>
+            <Footer/>
         </>
     );
 };
 
 export default TeacherProfile;
+
---
+++ src/pages/addCourse/add-course.tsx
@@ -0,0 +1,189 @@
+import Footer from "@/components/footer";
+import TeaNav from "@/components/teaNav";
+import { UploadIcon, PlusIcon, ChevronDownIcon } from "lucide-react";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+export default function AddCourse() {
+  const [selectedCategory, setSelectedCategory] = useState("Python");
+  const [classes, setClasses] = useState([{ id: 1 }]);
+
+  const handleCategoryChange = (event: any) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const addClass = () => {
+    const newClass = { id: classes.length + 1 };
+    setClasses([...classes, newClass]);
+  };
+
+  const removeClass = (id: number) => {
+    const newClasses = classes.filter((cls) => cls.id !== id);
+    setClasses(newClasses);
+  };
+
+  const handleFileUpload = (event: any, classId: number) => {
+    console.log(`Class ${classId} file selected:`, event.target.files[0]);
+  };
+
+  return (
+    <>
+      <TeaNav />
+      <div className="h-full w-full flex flex-col md:flex-row  px-20 py-12 max-sm:px-4 max-w-[1440px] mx-auto">
+        <div className="md:w-1/3 p-4 space-y-6 ">
+          <div className="mb-4">
+            <label htmlFor="title" className="block font-bold text-lg mb-2">
+              Başlık
+            </label>
+            <Input
+              type="text"
+              id="title"
+              placeholder="Kurs Başlığı"
+            />
+          </div>
+          <div className="flex gap-1 flex-wrap mb-4">
+            {['Python', 'Database', 'Graphic Design', 'AI',"ML"].map((cat) => (
+              <div key={cat} className="flex items-center space-x-2 space-y-2">
+                <input
+                  type="radio"
+                  name="options"
+                  id={cat}
+                  value={cat}
+                  checked={selectedCategory === cat}
+                  onChange={handleCategoryChange}
+                  className="hidden"
+                />
+                <label
+                  htmlFor={cat}
+                  className={`px-4 py-2 border rounded cursor-pointer ${selectedCategory === cat ? 'bg-[#D9D9D9] text-black' : 'bg-white text-black'}`}
+                >
+                  {cat}
+                </label>
+              </div>
+            ))}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="description" className="block font-bold text-lg mb-2">
+              Kurs Açıklaması
+            </label>
+            <textarea
+              id="description"
+              className="w-full border border-gray-300 rounded px-3 py-2"
+              placeholder="Kurs Açıklaması"
+              rows={4}
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="courseMedia" className="block font-bold text-lg mb-2">
+              Kurs Medyası
+            </label>
+            <div className="relative">
+              <input
+                type="file"
+                id="courseMedia"
+                className="hidden"
+                onChange={(e) => handleFileUpload(e, -1)}
+              />
+              <label
+                htmlFor="courseMedia"
+                className="flex items-center justify-center cursor-pointer border hover:bg-blue-700 hover:text-white border-gray-300 rounded px-3 py-2 bg-[#D9D9D9]"
+              >
+                <UploadIcon size={24} className="mr-2" />
+                Dosya Seç
+              </label>
+            </div>
+          </div>
+        </div>
+        <ScrollArea className="h-[520px] w-full rounded-md md:w-2/3 p-4 ">
+
+          <h2 className="text-lg font-bold mb-4">Sınıflar</h2>
+          {classes.map((cls) => (
+            <Collapsible key={cls.id} className="space-y-12 mb-4">
+              <div className="flex items-center justify-between space-x-4">
+                <h3 className="text-lg font-bold">Sınıf {cls.id}</h3>
+                <CollapsibleTrigger asChild>
+                  <Button size="icon" variant="ghost">
+                    <ChevronDownIcon className="h-5 w-5" />
+                    <span className="sr-only">Değiştir</span>
+                  </Button>
+                </CollapsibleTrigger>
+              </div>
+              <CollapsibleContent className="grid gap-4">
+                <div className="mb-4">
+                  <Label htmlFor={`className-${cls.id}`} className="font-bold text-lg">Sınıf Adı</Label>
+                  <Input
+                    type="text"
+                    id={`className-${cls.id}`}
+                    placeholder="Sınıf Adı"
+                  />
+                </div>
+                <div className="mb-4">
+                  <Label htmlFor={`classDesc-${cls.id}`} className="font-bold text-lg">Sınıf Açıklaması</Label>
+                  <textarea
+                    id={`classDesc-${cls.id}`}
+                    className="w-full border border-gray-300 rounded px-3 py-2"
+                    placeholder="Sınıf Açıklaması"
+                    rows={4}
+                  />
+                </div>
+                <div className="mb-4">
+                  <Label htmlFor={`classMedia-${cls.id}`} className="font-bold text-lg">Sınıf Medyası</Label>
+                  <div className="relative">
+                    <input
+                      type="file"
+                      id={`classMedia-${cls.id}`}
+                      className="hidden"
+                      onChange={(e) => handleFileUpload(e, cls.id)}
+                    />
+                    <label
+                      htmlFor={`classMedia-${cls.id}`}
+                      className="flex items-center justify-center cursor-pointer border hover:bg-blue-700 hover:text-white border-gray-300 rounded px-3 py-2 bg-[#D9D9D9]"
+                    >
+                      <UploadIcon size={24} className="mr-2" />
+                      Dosya Seç
+                    </label>
+                  </div>
+                </div>
+                <div className="mb-4">
+                  <Label htmlFor={`classMedia-${cls.id}`} className="font-bold text-lg">Sınıf Document</Label>
+                  <div className="relative">
+                    <input
+                      type="file"
+                      id={`classMedia-${cls.id}`}
+                      className="hidden"
+                      onChange={(e) => handleFileUpload(e, cls.id)}
+                    />
+                    <label
+                      htmlFor={`classMedia-${cls.id}`}
+                      className="flex items-center justify-center cursor-pointer border hover:bg-blue-700 hover:text-white border-gray-300 rounded px-3 py-2 bg-[#D9D9D9]"
+                    >
+                      <UploadIcon size={24} className="mr-2" />
+                      Dosya Seç
+                    </label>
+                  </div>
+                </div>
+                <Button size="sm" variant="outline" onClick={() => removeClass(cls.id)} className="bg-[#D9D9D9] text-black">
+                  Sınıfı Kaldır
+                </Button>
+              </CollapsibleContent>
+            </Collapsible>
+          ))}
+          <Button onClick={addClass} className="bg-[#0e4194]">
+            <PlusIcon className="h-4 w-4 mr-2" />
+            Başka Bir Sınıf Ekle
+          </Button>
+          <div className="mt-4 flex justify-center items-center">
+            <Button type="submit" variant="default" className="w-full bg-[#D9D9D9] text-black font-bold py-2 px-4 rounded lg:w-[40%] hover:bg-blue-700 hover:text-white">
+              Gönder
+            </Button>
+          </div>
+        </ScrollArea>
+      </div>
+      <Footer />
+    </>
+  );
+}
